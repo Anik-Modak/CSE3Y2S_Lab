c0da6810603dc1da6ce8e81231720d9e846890d8
Anik-Modak <anikmodak.rucse@gmail.com>
2019-11-08 20:51:50 +0600

    Added Client server program in java
    Added Client server program in java

---
+++ CSE3Y2S_Lab/DBMS/ServerProgram.java
@@ -0,0 +1,15 @@
+import java.io.*;  
+import java.net.*;  
+
+public class ServerProgram {  
+	public static void main(String[] args){  
+		try{  
+			ServerSocket ss=new ServerSocket(5555);  
+			Socket s=ss.accept();
+			DataInputStream dis=new DataInputStream(s.getInputStream());  
+			String  str=(String)dis.readUTF();  
+			System.out.println(" Message = "+str);  
+			ss.close();  
+		}catch(Exception e){System.out.println(e);}  
+	}  
+}  
